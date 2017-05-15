db4d65d3489c11f0b84d7fc18945836cfc07491c
Daniela Plascencia <daniela.plascencia@linux.intel.com>
2017-05-15 13:27:01 -0400

    Initial commit
    Signed-off-by: Daniela Plascencia <daniela.plascencia@linux.intel.com>

---
+++ mypypackage/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mypypackage',
+      version='0.1',
+      description='Hello world',
+      url='http://github.com/DnPlas/myPyPackage',
+      author='Daniela Plascencia',
+      author_email='daplascen@gmail.com',
+      license='MIT',
+      packages=['mypypackage'],
+      zip_safe=False)
